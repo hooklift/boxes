f8b451ab3bd17667b9051450e8800de8d76cd7a6
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-07-07 09:14:49 -0400

    Finishes coreos templates for alpha and beta channels
    Packer internal file HTTP server does not seem to be working, so we had to use github's public URL
    in order to specify the user-data yaml file.

--- coreos/scripts/install.sh
+++ coreos/scripts/install.sh
@@ -1,2 +1,2 @@
-curl https://raw.githubusercontent.com/c4milo/dobby-boxes/master/coreos/oem/config.yml > /tmp/cloudinit
-coreos-install -d /dev/sda -V ${OS_VERSION} -c /tmp/cloudinit
+curl https://raw.githubusercontent.com/c4milo/dobby-boxes/master/oem/config.yml > /tmp/cloudinit
+coreos-install -d /dev/sda -V current -C ${OS_VERSION} -c /tmp/cloudinit
